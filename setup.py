dbbb493c974bfbb382d6d544083e8ca2e4df1721
Lauro Cesar Ribeiro <lauroc.r.volei@hotmail.com>
2020-07-22 15:08:04 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Lauro Ribeiro Clinic',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Lauro Ribeiro',
+    author_email='lauroc.r.volei@hotmail.com',
+    description='Simple Crud in Python-Flask'
+)
